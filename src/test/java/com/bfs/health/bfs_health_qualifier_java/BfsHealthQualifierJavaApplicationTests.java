b43b41f4307948cd3343bce424838a5b97403153
Bhagawan Hemitha <behemitha44@gmail.com>
2025-08-29 11:45:04 +0530

    BFS Health Qualifier 1 solution

---
+++ src/test/java/com/bfs/health/bfs_health_qualifier_java/BfsHealthQualifierJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.health.bfs_health_qualifier_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfsHealthQualifierJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
